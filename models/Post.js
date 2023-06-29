648f29ce295986412d342578f27489ca8d97e699
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2023-06-29 17:43:49 +0100

    ft--models updated

--- models/Post.js
+++ models/Post.js
@@ -1,6 +1,28 @@
 import { Schema, model, models } from 'mongoose'
 
-const PostSchema = new Schema({})
+const PostSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      max: 20,
+    },
+    description: {
+      type: String,
+      max: 500,
+    },
+    comments: {
+      type: Array,
+      default: [],
+    },
+  },
+  {
+    timestamps: trues,
+  }
+)
 
 const Post = models.Post || model('Post', PostSchema)
 
--- models/User.js
+++ models/User.js
@@ -1,6 +1,37 @@
 import { Schema, model, models } from 'mongoose'
 
-const UserSchema = new Schema({})
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 20,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+    profileImage: {
+      type: String,
+      default: '',
+    },
+    description: {
+      type: String,
+      max: 50,
+    },
+    isAdmin: { type: Boolean, required: true, default: false },
+  },
+  { timestamps: true }
+)
 
 const User = models.User || model('User', UserSchema)
 
