43320dd8129e5ed22b36256b4ea76b45592ca8ad
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2023-06-30 12:00:45 +0100

    ft--user login api added

---
+++ pages/api/users/login.js
@@ -0,0 +1,33 @@
+import bcrypt from 'bcryptjs'
+import User from '../../../models/User'
+import db from '../../../utils/db'
+import { signToken } from '../../../utils/auth'
+
+export default async function login(req, res) {
+  const { username, password } = req.body
+  try {
+    if (!password || typeof password !== 'string') {
+      throw new Error('Password is required') // Add validation for the password
+    }
+    await db.connect()
+    const user = await User.findOne({ username })
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const token = signToken(user)
+      res.status(200).json({
+        token,
+        _id: user._id,
+        email: user.email,
+        username: user.username,
+        message: 'User Login successful',
+      })
+    } else {
+      res.status(401).json({ message: 'Invalid username or password' })
+    }
+  } catch (error) {
+    console.log(errors)
+    res.status(500).json({
+      success: false,
+      message: 'An error occurred logging the user',
+    })
+  }
+}
