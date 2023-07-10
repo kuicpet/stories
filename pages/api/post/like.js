e54ea228e0523f55d333e44d275fc0ae71493868
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2023-07-10 23:46:48 +0100

    ft--like api updated

--- pages/api/post/like.js
+++ pages/api/post/like.js
@@ -1,15 +1,17 @@
 import db from '../../../utils/db'
 import Post from '../../../models/Post'
+import User from '../../../models/User'
 
 export default async function toggleLikePost(req, res) {
   if (req.method === 'POST') {
     try {
       await db.connect()
       const { postId, userId } = req.body
-      const post = await Post.findById(postId)
+      const post = await Post.findById(postId).populate('likes', 'username')
       if (!post) {
         return res.status(404).json({ success: false, error: 'Post not found' })
       }
+
       const userIndex = post.likes.findIndex((id) => id.toString() === userId)
 
       if (userIndex === -1) {
@@ -21,7 +23,14 @@ export default async function toggleLikePost(req, res) {
       }
 
       const updatedPost = await post.save()
-      res.status(201).json({ success: true, date: updatedPost })
+
+      res.status(201).json({
+        success: true,
+        date: {
+          post: updatedPost,
+          likedUsers: updatedPost.likes.map((user) => user.username),
+        },
+      })
     } catch (error) {
       console.error(error)
       res.status(500).json({ success: false, error: 'Failed to like on post' })
