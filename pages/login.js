774f3bf3cc666cda543de3ab7f5ff4ffe93d7538
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2023-07-13 12:48:01 +0100

    ft--authentication intergrated with backend

--- pages/login.js
+++ pages/login.js
@@ -1,18 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
+import { toast, Toaster } from 'react-hot-toast'
+import axios from 'axios'
 
-const login = () => {
+const Login = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      await axios
+        .post(`/api/user/login`, { username, password })
+        .then((response) => {
+          if (response.status === 200) {
+            toast.success(response?.data?.message)
+            // console.log(response)
+            // save user
+            // redirect user
+          } else {
+            toast.error(response?.data?.message)
+            return
+          }
+        })
+    } catch (error) {
+      // console.log(error)
+      toast.error(error?.response?.data?.message)
+    }
+  }
+
   return (
     <section className='flex items-center justify-center border border-black lg:w-1/2 w-[90%] m-5 p-2'>
+      <Toaster />
       <div className='lg:w-[90%]'>
         <div className='flex items-center justify-center'>
-          <h1 className='text-xl font-semibold'>Login to Share your Stories</h1>
+          <h1 className='text-xl font-semibold'>Login to share your Stories</h1>
         </div>
-        <form className='m-3 p-2'>
+        <form className='m-3 p-2' onSubmit={handleSubmit}>
           <div className='mb-2'>
             <label>Username</label>
             <input
               type='text'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
               placeholder='Enter your Username'
               className='border border-black w-full p-1.5'
             />
@@ -21,12 +50,16 @@ const login = () => {
             <label>Password</label>
             <input
               type='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               placeholder='Password'
               className='border border-black w-full p-1.5'
             />
           </div>
           <div className='my-7'>
-            <button className='w-full border border-black p-2'>Login</button>
+            <button type='submit' className='w-full border border-black p-2'>
+              Login
+            </button>
           </div>
         </form>
         <div className='flex justify-center mb-2'>
@@ -42,4 +75,4 @@ const login = () => {
   )
 }
 
-export default login
+export default Login
--- pages/signup.js
+++ pages/signup.js
@@ -1,20 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useRouter } from 'next/router'
 import Link from 'next/link'
+import { toast, Toaster } from 'react-hot-toast'
+import axios from 'axios'
 
-const signup = () => {
+const Signup = () => {
+  const router = useRouter()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [username, setUsername] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    if (password !== confirmPassword) {
+      toast.error('Passwords donot match')
+      return
+    }
+    try {
+      await axios
+        .post(`/api/user/register`, {
+          username,
+          email,
+          password,
+        })
+        .then((response) => {
+          if (response.status === 201) {
+            toast.success(response?.data?.message)
+            // save user
+            // redirect user
+            router.push(`/login`)
+          } else {
+            toast.error(response?.data?.message)
+            return
+          }
+        })
+    } catch (error) {
+      console.log(error)
+      toast.error(error?.response?.data?.message)
+    }
+  }
+
   return (
     <section className='flex items-center justify-center border border-black lg:w-1/2 w-[90%] m-5 p-2'>
+      <Toaster />
       <div className='lg:w-[90%]'>
         <div className='flex items-center justify-center'>
           <h1 className='text-xl font-semibold'>
-            Sign up to Share your Stories
+            Sign up to share your Stories
           </h1>
         </div>
-        <form className='m-3 p-2'>
+        <form className='m-3 p-2' onSubmit={handleSubmit}>
           <div className='mb-2'>
             <label>Username</label>
             <input
               type='text'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
               placeholder='Enter your Username'
               className='border border-black w-full p-1.5'
             />
@@ -22,7 +64,9 @@ const signup = () => {
           <div className='mb-2'>
             <label>Email</label>
             <input
-              type='text'
+              type='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               placeholder='Email'
               className='border border-black w-full p-1.5'
             />
@@ -31,6 +75,8 @@ const signup = () => {
             <label>Password</label>
             <input
               type='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               placeholder='Password'
               className='border border-black w-full p-1.5'
             />
@@ -39,12 +85,16 @@ const signup = () => {
             <label>Confirm Password</label>
             <input
               type='password'
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
               placeholder='Confirm password'
               className='border border-black w-full p-1.5'
             />
           </div>
           <div className='my-7'>
-            <button className='w-full border border-black p-2'>Sign up</button>
+            <button type='submit' className='w-full border border-black p-2'>
+              Sign up
+            </button>
           </div>
         </form>
         <div className='flex justify-center mb-2'>
@@ -60,4 +110,4 @@ const signup = () => {
   )
 }
 
-export default signup
+export default Signup
