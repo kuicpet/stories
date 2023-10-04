8e0e7dcafcb0aefff19b999aac7844242a5f167d
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2023-10-04 12:15:51 +0100

    ft--responsiveness updated for admin dashboard

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -3,9 +3,10 @@ import Link from 'next/link'
 import React, { useState, useEffect } from 'react'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
 import { formatTimestamp } from '../../utils/formatTimestamp'
+import { Loader } from '../../components'
 
 const AdminDashboard = () => {
-  const [userList, setuserList] = useState([])
+  const [users, setUsers] = useState([])
   const [posts, setPosts] = useState([])
   const [tabIndex, setTabIndex] = useState(0)
   const [loading, setLoading] = useState(false)
@@ -24,8 +25,8 @@ const AdminDashboard = () => {
       await axios.get('/api/post').then((response) => {
         if (response.status === 200) {
           setLoading(false)
-          setPosts(response.data)
-          console.log(response.data)
+          setPosts(response?.data)
+          // console.log(response.data, 'admin')
         }
       })
     } catch (error) {
@@ -35,36 +36,80 @@ const AdminDashboard = () => {
     }
   }
 
-  const fetchUsers = () => {}
+  const fetchUsers = async () => {
+    try {
+      setLoading(true)
+      await axios.get('/api/admin/user').then((response) => {
+        if (response.status === 200) {
+          setLoading(false)
+          setUsers(response?.data)
+          // console.log(response?.data)
+        }
+      })
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setLoading(false)
+    }
+  }
 
   return (
-    <section className='flex w-[95%] p-2 border-2 border-black m-2'>
-      <div className='flex flex-col w-1/4 border border-black p-1 h-[92vh]'>
-        <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+    <section className='flex flex-col md:flex-row w-[95%] p-2 border-2 border-black m-2'>
+      {loading && (
+        <div className='absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg'>
+          <Loader />
+        </div>
+      )}
+      <div className='flex flex-col w-1/4 border border-black p-1 md:h-[92vh]'>
+        <Tabs
+          selectedIndex={tabIndex}
+          onSelect={(index) => setTabIndex(index)}
+          className='border-none outline-none'>
           <TabList>
-            <Tab className='border-2 p-1 cursor-pointer border-black'>
+            <Tab
+              className={`${
+                tabIndex === 0 ? 'bg-[#ace5d4]' : 'hover:bg-[#ace5d4]'
+              } border-2 p-1 cursor-pointer border-black outline-none`}>
               Stories
             </Tab>
-            <Tab className='border-2 p-1 cursor-pointer border-black'>
+            <Tab
+              className={`${
+                tabIndex === 1 ? 'bg-[#ace5d4]' : 'hover:bg-[#ace5d4]'
+              } border-2 p-1 cursor-pointer border-black outline-none`}>
               Users
             </Tab>
           </TabList>
         </Tabs>
       </div>
-      <div className='w-3/4 border border-black p-1'>
+      <div className='md:w-3/4 border border-black p-1'>
         <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
           <TabPanel>
-            <div className='border-2 border-black p-1'>
-              <div className='border-b-2 border-black'>
+            <div className='border-2 border-black p-1 overflow-x-auto'>
+              <div className='w-full'>
                 <p>Stories</p>
               </div>
-              <table className='w-full text-left'>
-                <thead className='border-b-2 border-black'>
+              <table className='min-w-full border-2 border-black divide-y text-left uppercase'>
+                <thead className='border-b-2 border-black bg-[#ace5d4]'>
                   <tr>
+                    {/**
+                     * 
                     <th>PostId</th>
-                    <th>User</th>
-                    <th>Title</th>
-                    <th>createdAt</th>
+                     */}
+                    <th
+                      scope='col'
+                      className='text-xs md:text-[1rem]  py-1 font-medium tracking-wider'>
+                      Date
+                    </th>
+                    <th
+                      scope='col'
+                      className='flex text-xs md:text-[1rem]  py-1 font-medium tracking-wider'>
+                      <span className='hidden md:block mr-1'>Created </span> By
+                    </th>
+                    <th
+                      scope='col'
+                      className='text-xs md:text-[1rem]  py-1 font-medium tracking-wider'>
+                      Title
+                    </th>
                   </tr>
                 </thead>
                 {posts &&
@@ -72,10 +117,17 @@ const AdminDashboard = () => {
                   posts?.map((post) => (
                     <tbody key={post._id} className=''>
                       <tr className='border-t border-black'>
+                        {/**
+                         * 
                         <td>{post._id.slice(0, 6)}...</td>
-                        <td>{post?.userId?.username}</td>
-                        <td>{post.title}</td>
-                        <td>{formatTimestamp(post?.createdAt)}</td>
+                         */}
+                        <td className='whitespace-nowrap'>
+                          {formatTimestamp(post?.createdAt)}
+                        </td>
+                        <td className='whitespace-nowrap'>
+                          {post?.userId?.username}
+                        </td>
+                        <td className='whitespace-nowrap'>{post.title}</td>
                       </tr>
                     </tbody>
                   ))}
@@ -83,8 +135,52 @@ const AdminDashboard = () => {
             </div>
           </TabPanel>
           <TabPanel>
-            <div className='border-2 border-black p-1'>
-              <p>Users Content</p>
+            <div className='border-2 border-black p-1 overflow-x-auto'>
+              <div className='w-full'>
+                <p>Users</p>
+              </div>
+              <table className='min-w-full border-2 border-black divide-y text-left uppercase'>
+                <thead className='border-b-2 border-black bg-[#ace5d4]'>
+                  <tr>
+                    {/**
+                     * 
+                    <th>PostId</th>
+                     */}
+                    <th
+                      scope='col'
+                      className='text-xs md:text-[1rem]  py-1 font-medium tracking-wider'>
+                      Date Joined
+                    </th>
+                    <th
+                      scope='col'
+                      className='text-xs md:text-[1rem]  py-1 font-medium tracking-wider'>
+                      username
+                    </th>
+                    <th
+                      scope='col'
+                      className='text-xs md:text-[1rem]  py-1 font-medium tracking-wider'>
+                      email
+                    </th>
+                  </tr>
+                </thead>
+                {users &&
+                  users.length > 0 &&
+                  users?.map((user) => (
+                    <tbody key={user._id} className=''>
+                      <tr className='border-t border-black'>
+                        {/**
+                         * 
+                        <td>{post._id.slice(0, 6)}...</td>
+                         */}
+                        <td className='whitespace-nowrap'>
+                          {formatTimestamp(user?.createdAt)}
+                        </td>
+                        <td className='whitespace-nowrap'>{user?.username}</td>
+                        <td className='whitespace-nowrap'>{user?.email}</td>
+                      </tr>
+                    </tbody>
+                  ))}
+              </table>
             </div>
           </TabPanel>
         </Tabs>
